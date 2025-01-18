588c26abae8b57e817d744c306111bf63d5738c7
Kartik Pawar <kartikpawar1290@gmail.com>
2025-01-18 13:23:56 +0530

    Home page
    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/GlobalNavbar';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
 import Blog from './components/Blog';
@@ -8,18 +9,28 @@ import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 import FAQ from './components/FAQ';
 import Services from './components/Services';
+import HeroSection from './components/HeroSection';
+import Services2 from './components/Services2';
+import Locations from './components/Locations';
+import Testimonials from './components/Testimonials';
 
-import "./App.css"
-import Footer from './components/Footer';
-
-
+function MainContent() {
+  return (
+    <div style={{ margin: 0, padding: 0, fontFamily: 'Poppins, sans-serif' }}>
+      <HeroSection />
+      <Services2 />
+      <Locations />
+      <Testimonials />
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainContent />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
