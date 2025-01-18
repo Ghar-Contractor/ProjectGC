724b2adbe530a766921f9e5c11bcc23d9fd2620b
Gautami-11 <gautami1154@gmail.com>
2025-01-18 16:50:49 +0530

    contact page changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/GlobalNavbar';
 import Footer from './components/Footer';
-import Home from './components/Home';
+import Home from './components/Home.jsx';
 import About from './components/About';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
